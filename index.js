4c2a3b45e8e2a3a06d0cdb45a8d685889b74acf1
Lexx23021074 <lexx23021074@gmail.com>
2026-06-20 10:05:40 +0000

    Deploying to gh-pages from @ Lexx23021074/Blended_12@9e54ebaf7cb3c4bda3d7d98affa738f1325c7e3e 🚀

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import"./assets/styles-JE8YjOlG.js";import{a as e}from"./assets/vendor-N5iQpiFS.js";const u=()=>{const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")},g="https://dummyjson.com",r={categories:"/products/category-list",pages:"/products"},o=12;e.defaults.baseURL=g;const _=async()=>(await e.get(r.categories)).data,m=async t=>(await e.get(r.pages,{params:{limit:o,skip:(t-1)*o}})).data,a={categories:document.querySelector(".categories"),products:document.querySelector(".products")},y=t=>{const s=t.map(c=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${c}</button>
+import"./assets/styles-JE8YjOlG.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";const _=()=>{const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")},y=t=>{const s=document.querySelector(".categories__btn--active");s&&s.classList.remove("categories__btn--active"),t.classList.add("categories__btn--active")},m="https://dummyjson.com",a={categories:"/products/category-list",pages:"/products",productsByCategory:"/products/category"},n=12;o.defaults.baseURL=m;const b=async()=>(await o.get(a.categories)).data,i=async t=>(await o.get(a.pages,{params:{limit:n,skip:(t-1)*n}})).data,f=async t=>(await o.get(`${a.productsByCategory}/${t}`)).data,r={categories:document.querySelector(".categories"),products:document.querySelector(".products")},P=t=>{const s=t.map(e=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${e}</button>
  </li>
-`).join("");a.categories.innerHTML=s},b=t=>{const s=t.map(({title:c,brand:n,category:i,price:p,id:d,thumbnail:l})=>`<li class="products__item" data-id="${d}">
-    <img class="products__image" src="${l}" alt="${c}"/>
-    <p class="products__title">${c}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:</span> ${n}</p>
-    <p class="products__category">Category: ${i}</p>
-    <p class="products__price">Price: $${p}</p>
+`).join("");r.categories.innerHTML=s},d=t=>{const s=t.map(({title:e,brand:c,category:l,price:u,id:p,thumbnail:g})=>`<li class="products__item" data-id="${p}">
+    <img class="products__image" src="${g}" alt="${e}"/>
+    <p class="products__title">${e}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:</span> ${c}</p>
+    <p class="products__category">Category: ${l}</p>
+    <p class="products__price">Price: $${u}</p>
  </li>
-`).join("");a.products.insertAdjacentHTML("beforeend",s)},$=async()=>{try{const t=await _();y(["all",...t]),u()}catch(t){console.log(t)}},f=async()=>{try{const t=await m(1);console.log(t),b(t.products)}catch(t){console.log(t)}};$();f();
+`).join("");r.products.insertAdjacentHTML("beforeend",s)},$=()=>{r.products.innerHTML=""},B=async()=>{try{const t=await b();P(["all",...t]),_()}catch(t){console.log(t)}},C=async()=>{try{const t=await i(1);console.log(t),d(t.products)}catch(t){console.log(t)}},v=async t=>{if(t.target.nodeName!=="BUTTON")return;const s=t.target.textContent;let e=null;try{s==="all"?e=(await i(1)).products:e=(await f(s)).products,$(),d(e),y(t.target)}catch(c){console.log(c)}};B();C();r.categories.addEventListener("click",v);
 //# sourceMappingURL=index.js.map
